de685402c5aeef7bd0fa60b17af5070aca728f16
HumansDoNotWantImmortality <rosinski.krzysztof.01@gmail.com>
2023-06-07 16:22:33 +0000

    Update at 2023-06-07T16:22:33.903823Z

--- src/main/java/com/bytelegend/Computer.java
+++ src/main/java/com/bytelegend/Computer.java
@@ -20,70 +20,16 @@ public class Computer {
     /** The battery model, optional. */
     private final String battery;
 
-    public Computer(String cpu, String ram, String motherboard) {
-        this(cpu, ram, motherboard, null);
-    }
-
-    public Computer(String cpu, String ram, String motherboard, String storage) {
-        this(cpu, ram, motherboard, storage, null);
-    }
-
-    public Computer(String cpu, String ram, String motherboard, String storage, String display) {
-        this(cpu, ram, motherboard, storage, display, null);
-    }
-
-    public Computer(
-            String cpu,
-            String ram,
-            String motherboard,
-            String storage,
-            String display,
-            String keyboard) {
-        this(cpu, ram, motherboard, storage, display, keyboard, null);
-    }
-
-    public Computer(
-            String cpu,
-            String ram,
-            String motherboard,
-            String storage,
-            String display,
-            String keyboard,
-            String mouse) {
-        this(cpu, ram, motherboard, storage, display, keyboard, mouse, null);
-    }
-
-    public Computer(
-            String cpu,
-            String ram,
-            String motherboard,
-            String storage,
-            String display,
-            String keyboard,
-            String mouse,
-            String speaker) {
-        this(cpu, ram, motherboard, storage, display, keyboard, mouse, speaker, null);
-    }
-
-    public Computer(
-            String cpu,
-            String ram,
-            String motherboard,
-            String storage,
-            String display,
-            String keyboard,
-            String mouse,
-            String speaker,
-            String battery) {
-        this.cpu = cpu;
-        this.ram = ram;
-        this.motherboard = motherboard;
-        this.storage = storage;
-        this.display = display;
-        this.keyboard = keyboard;
-        this.mouse = mouse;
-        this.speaker = speaker;
-        this.battery = battery;
+    public Computer(ComputerBuilder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.motherboard = builder.motherboard;
+        this.storage = builder.storage;
+        this.display = builder.display;
+        this.keyboard = builder.keyboard;
+        this.mouse = builder.mouse;
+        this.speaker = builder.speaker;
+        this.battery = builder.battery;
     }
 
     public String getCpu() {
@@ -164,8 +110,54 @@ public class Computer {
     }
 
     public static final class ComputerBuilder {
+        private final String cpu;
+        private final String ram;
+        private final String motherboard;
+        private String storage;
+        private String display;
+        private String keyboard;
+        private String mouse;
+        private String speaker;
+        private String battery;
+
+        private ComputerBuilder(String cpu, String ram, String motherboard) {
+            this.cpu = cpu;
+            this.ram = ram;
+            this.motherboard = motherboard;
+        }
+
+        public ComputerBuilder withStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public ComputerBuilder withDisplay(String display) {
+            this.display = display;
+            return this;
+        }
+
+        public ComputerBuilder withKeyboard(String keyboard) {
+            this.keyboard = keyboard;
+            return this;
+        }
+
+        public ComputerBuilder withMouse(String mouse) {
+            this.mouse = mouse;
+            return this;
+        }
+
+        public ComputerBuilder withSpeaker(String speaker) {
+            this.storage = speaker;
+            return this;
+        }
+
+        public ComputerBuilder withBattery(String battery) {
+            this.battery = battery;
+            return this;
+        }
+
         public Computer build() {
-            return new Computer();
+            return new Computer(this);
         }
     }
 }
